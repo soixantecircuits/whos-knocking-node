d81a2510ccd92b14bd4c91503d6a54cc3a93cc09
Gabriel Delattre <gabriel@soixantecircuits.fr>
2012-09-09 16:57:13 +0200

    modify server to handle right twit

--- app.js
+++ app.js
@@ -20,26 +20,24 @@ var t = new twitter({
     access_token_key: credentials.access_token_key,
     access_token_secret: credentials.access_token_secret
 });
-//io.sockets.on('connection', function (socket) {
-	t.stream(
-	    'statuses/filter',
-	    { track: ['awesome', 'cool', 'rad', 'gnarly', 'groovy'] },
-	    function(stream) {
-	        stream.on('data', function(tweet) {
-	            //console.log(tweet.text);
-	            nTwitterCount++;
-	            new_tweet = tweet.text;
- 				console.log(tweet.text);
-            	io.sockets.emit('new_tweet', new_tweet);
-	        });
-	        stream.on('error', function (response){
-	        	console.log(response);
-	        });
-	        stream.on('end', function (response) {
-				console.log(response);
-		  	});
-		  	stream.on('destroy', function (response) {
-		    	console.log(response);
-  			});
-	});
-//});
+t.stream(
+    'statuses/filter',
+    { track: ['soixantecircuits', 'ringthebell'] },
+    function(stream) {
+        stream.on('data', function(tweet) {
+            //console.log(tweet.text);
+            nTwitterCount++;
+            new_tweet = tweet.text;
+				console.log(tweet.text);
+        	io.sockets.emit('new_tweet', new_tweet);
+        });
+        stream.on('error', function (response){
+        	console.log(response);
+        });
+        stream.on('end', function (response) {
+			console.log(response);
+	  	});
+	  	stream.on('destroy', function (response) {
+	    	console.log(response);
+			});
+});
