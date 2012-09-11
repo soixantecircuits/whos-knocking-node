2d8365b29a19cd90c49f39a7b6dc8d69343a2c05
Gabriel Delattre <gabriel@soixantecircuits.fr>
2012-09-11 08:19:54 +0200

    adding debug info

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var twitter = require('ntwitter');
 var credentials = require('./credentials.js');
-var http = require('http');
-var port = process.env.PORT || 5000;
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 
+var port = process.env.PORT || 5000;
 var nTwitterCount = 0;
 
-var httpServer = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Ring the Bell\n'+nTwitterCount);
-}).listen(port);
+server.listen(port);
 
-var io = require('socket.io').listen(httpServer);
+app.get('/', function(req, res) {
+    //res.sendfile(__dirname + '/index.html');
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Ring the Bell\n' + nTwitterCount);
+});
+
 
 var new_tweet = {};
 
@@ -20,24 +26,23 @@ var t = new twitter({
     access_token_key: credentials.access_token_key,
     access_token_secret: credentials.access_token_secret
 });
-t.stream(
-    'statuses/filter',
-    { track: ['soixantecircuits', 'ringthebell'] },
-    function(stream) {
-        stream.on('data', function(tweet) {
-            //console.log(tweet.text);
-            nTwitterCount++;
-            //new_tweet = tweet.text;
-			console.log(tweet);
-        	io.sockets.emit('new_tweet', tweet);
-        });
-        stream.on('error', function (response){
-        	console.log(response);
-        });
-        stream.on('end', function (response) {
-			console.log(response);
-	  	});
-	  	stream.on('destroy', function (response) {
-	    	console.log(response);
-			});
+t.stream('statuses/filter', {
+    track: ['soixantecircuits', 'ringthebell']
+}, function(stream) {
+    stream.on('data', function(tweet) {
+        //console.log(tweet.text);
+        nTwitterCount++;
+        //new_tweet = tweet.text;
+        console.log(tweet);
+        io.sockets.emit('new_tweet', tweet);
+    });
+    stream.on('error', function(response) {
+        console.log(response);
+    });
+    stream.on('end', function(response) {
+        console.log(response);
+    });
+    stream.on('destroy', function(response) {
+        console.log(response);
+    });
 });
