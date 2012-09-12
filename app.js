37e837e5761bcd6f26b2dc99cffb023905f42ab4
Gabriel Delattre <gabriel@soixantecircuits.fr>
2012-09-13 01:48:29 +0200

    switching to long polling

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ var app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
 
+io.configure(function() {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+});
+
 var port = process.env.PORT || 5000;
 var nTwitterCount = 0;
 
