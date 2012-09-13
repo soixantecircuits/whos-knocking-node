b9ef21f50cfaa708426299c602a1ca0542fe5de0
Gabriel Delattre <gabriel@soixantecircuits.fr>
2012-09-13 10:07:35 +0200

    check for creds

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ try {
 } catch (err) {
     console.log("Error:", err);
     console.log("Using environmental credentials.");
-    var credentials = {
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token_key: process.env.ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
-    };
+    if (process.env.CONSUMER_KEY != "") {
+        var credentials = {
+            consumer_key: process.env.CONSUMER_KEY,
+            consumer_secret: process.env.CONSUMER_SECRET,
+            access_token_key: process.env.ACCESS_TOKEN_KEY,
+            access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        };
+    } else {
+        exit("no creds");
+    }
 }
 
 var app = require('express')(),
